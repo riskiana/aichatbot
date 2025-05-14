8f310e6aa4ce577c4e0d305086fd66cb7baaf541
Riskiana Wulan <riskiana.wulan@kuelap.io>
2025-05-14 15:13:24 +0700

    init

---
+++ src/main/java/com/riskiana/aichatbot/AichatbotApplication.java
@@ -0,0 +1,13 @@
+package com.riskiana.aichatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AichatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riskiana/aichatbot/AichatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskiana.aichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
